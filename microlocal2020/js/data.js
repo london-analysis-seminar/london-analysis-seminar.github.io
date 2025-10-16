a1fcec84c7f2cc0a44431a6cde25f162d41f9226
pushnitski <pushnitski@gmail.com>
2025-10-16 18:10:05 +0100

    Initial website upload
    Upload of the files that were previously stored on 123reg.co.uk

---
+++ microlocal2020/js/data.js
@@ -0,0 +1,22 @@
+var data = [
+	{
+		"name": "London Analysis Seminar",
+		"gcal": "london.analysis@gmail.com"
+	},
+	{
+		"name": "Inverse Problems Seminar",
+		"gcal": "liq65gff82s9mr5ip1ss8g3ads@group.calendar.google.com"
+	},
+	{
+		"name": "King's College Analysis Seminar",
+		"gcal": "gkij4q9m1249c2osijddav6dig@group.calendar.google.com"
+	},
+	{
+		"name": "Imperial Pure Analysis Seminar",
+		"gcal": "imperial.pure.analysis@gmail.com"
+	},
+	{
+		"name": "KCL-UCL Geometry Seminar",
+		"gcal": "giu2ael3iq4sd9ucqa2uur7048@group.calendar.google.com"
+	}
+]
